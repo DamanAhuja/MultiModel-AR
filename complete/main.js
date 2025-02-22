705cd066a29e7fdbecf67b7023964f8371df326c
Daman Ahuja <142963733+DamanAhuja@users.noreply.github.com>
2025-02-22 14:12:34 +0530

    Update main.js

--- complete/main.js
+++ complete/main.js
@@ -1,6 +1,7 @@
 import * as THREE from "../libs/three123/three.module.js";
 import { ARButton } from "../libs/jsm/ARButton.js";
-import { GLTFLoader } from "../libs/jsm/GLTFLoader.js";
+import { GLTFLoader } from "../libs/jsm/loaders/GLTFLoader.js";
+
 
 const loader = new GLTFLoader();
 // Utility functions
